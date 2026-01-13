428de22d9494978b362656303dda6d93c625c950
MUHAMMADULLAH1991 <muhammadullahsaify859658@gmail.com>
2026-01-13 21:09:40 +0600

    Intro to conditional statement

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+let rahim;
+let karim;
+let rafiq;
+
+rahim = "cycle";
+karim = "car";
+rafiq = "walk";
+
+if(rahim == 'cycle'){
+    console.log("I will go to park");
+}
+else if(karim == 'car'){
+    console.log("I will go to shopping");
+}
+else if(rafiq == 'walk'){
+    console.log("We will recite holy Quran");
+}
+else{
+    console.log("I wil study");
+}
